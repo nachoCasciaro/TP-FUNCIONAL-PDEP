23dec05997da93c12f1d21dbaaf207cc348b67cc
nacho casciaro <icasciaro@est.frba.utn.edu.ar>
2018-04-30 21:32:00 -0300

    terminado
    falta preguntar nop y casos de prueba

--- microprocesadores.hs
+++ microprocesadores.hs
@@ -44,11 +44,17 @@ programaQueSume10Con22 valor1 valor2 unMicroprocesador = (add.(lodv valor2).swap
 
 --1.--
 
-divide unMicroprocesador = unMicroprocesador {acumuladorA = div (acumuladorA unMicroprocesador) (acumuladorB unMicroprocesador) , acumuladorB = 0, programCounter = programCounter unMicroprocesador +1}
---divide unMicroprocesador = unMicroprocesador {acumuladorA = acumuladorA unMicroprocesador / acumuladorB unMicroprocesador , acumuladorB = 0, programCounter = programCounter unMicroprocesador +1}
+divide :: MicroProcesador->MicroProcesador
+divide unMicroprocesador
+      |(acumuladorB unMicroprocesador) /= 0 =  unMicroprocesador {acumuladorA = div (acumuladorA unMicroprocesador) (acumuladorB unMicroprocesador) , acumuladorB = 0, programCounter = programCounter unMicroprocesador +1}
+      |otherwise = unMicroprocesador {etiqueta = "division by zero",programCounter =  programCounter unMicroprocesador +1 }
 
+str :: Int->Int->MicroProcesador->MicroProcesador
 str addr val unMicroprocesador = unMicroprocesador { memoria = (take (addr-1) (memoria unMicroprocesador)) ++ [val] ++ (drop (addr-1) (memoria unMicroprocesador)), programCounter = programCounter unMicroprocesador +1}
 
+lod :: Int->MicroProcesador->MicroProcesador
 lod addr unMicroprocesador = unMicroprocesador { acumuladorA = (!!) (memoria unMicroprocesador) (addr-1), programCounter =  programCounter unMicroprocesador +1}
 
 --2.--
+
+programaQueDivide2Por0 valor1 valor2 addr1 addr2 unMicroprocesador  = (divide.(lod addr1).swap.(lod addr2).(str addr2 valor1) .(str addr1 valor2)) unMicroprocesador
