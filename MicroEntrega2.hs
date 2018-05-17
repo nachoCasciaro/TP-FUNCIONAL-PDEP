a90051a48acbb3dead5e84e4fecb616d999437f6
nacho casciaro <icasciaro@est.frba.utn.edu.ar>
2018-05-17 20:43:48 -0300

    agregando funciones del tp 1

--- MicroEntrega2.hs
+++ MicroEntrega2.hs
@@ -1,7 +1,6 @@
-module MicroEntrega1 where
+module MicroEntrega2 where
 import Test.Hspec
 import Text.Show.Functions
-
 
 
 --3.1 punto 1--
@@ -73,6 +72,7 @@ lod addr unMicroprocesador = aumentarPC unMicroprocesador { acumuladorA = (!!) (
 --3.1 Punto 1--
 
 type Programa = [(MicroProcesador -> MicroProcesador)]
+
 cargarPrograma :: Programa -> MicroProcesador -> MicroProcesador
 cargarPrograma programa unMicroprocesador = unMicroprocesador { programas = programas unMicroprocesador ++ [programa] }
 
@@ -83,24 +83,28 @@ programaQueDivide2Por0 = [(str 1 2), (str 2 0), (lod 2), swap, (lod 1), divide]
 
 --3.2 Punto 2--
 ejecutarPrograma :: Programa->MicroProcesador->MicroProcesador
-ejecutarPrograma [ ] unMicroProcesador = unMicroProcesador
+ejecutarPrograma [] unMicroProcesador = unMicroProcesador
 ejecutarPrograma (x : xs) (UnMicroprocesador memoria acumuladorA acumuladorB programCounter [] programas) = ( ejecutarPrograma xs . x ) (UnMicroprocesador memoria acumuladorA acumuladorB programCounter [] programas)
 ejecutarPrograma _ unMicroProcesador = unMicroProcesador
 
 --3.3 Punto 3--
 
 ifnz :: Programa->MicroProcesador->MicroProcesador
-ifnz [ ] unMicroProcesador = unMicroProcesador
+ifnz [] unMicroProcesador = unMicroProcesador
 ifnz _ (UnMicroprocesador memoria 0 acumuladorB programCounter mensajeError programas) = (UnMicroprocesador memoria 0 acumuladorB programCounter mensajeError programas)
 ifnz programa unMicroProcesador = ejecutarPrograma programa unMicroProcesador
 
-  --3.4 Punto 4--
-prueba = [swap, nop, lodv 133, lodv 0, str 1 3, str 2 0]
+--3.4 Punto 4--
+type Instruccion = (MicroProcesador->MicroProcesador)
+prueba = [swap,nop,lodv 133,lodv 0,str 1 3,str 2 0]
+
 depurarPrograma :: Programa -> MicroProcesador -> Programa
 depurarPrograma programa unMicro = filter (condicion unMicro) programa
 
---condicion :: MicroProcesador->(MicroProcesador->MicroProcesador)->MicroProcesador
+condicion :: MicroProcesador->Instruccion->Bool
 condicion unMicro instruccion = ( controlar. instruccion) unMicro
 
---controlar :: MicroProcesador->Bool
+controlar :: MicroProcesador->Bool
 controlar unMicro = acumuladorA unMicro /= 0 || acumuladorB unMicro /= 0 || memoria unMicro /= []
+
+--
